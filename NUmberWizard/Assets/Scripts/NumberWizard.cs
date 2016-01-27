25af9dd8697564687ac346bff41dcca500e4244f
JackDraak <jackdraak@gmail.com>
2016-01-27 18:18:43 -0500

    ready for lecture 39, setting up Unity

--- NUmberWizard/Assets/Scripts/NumberWizard.cs
+++ NUmberWizard/Assets/Scripts/NumberWizard.cs
@@ -2,14 +2,62 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
+	int RangeTop;
+	int RangeBottom;
+	bool bGameOver;
+	int CurrentGuess;
 
-	// Use this for initialization
 	void Start () {
-        print ("Welcome to Number Wizard");
+        	StartNWGame ();
+    	}
+
+	void StartNWGame ()
+	{
+		RangeTop = 1000;
+		RangeBottom = 1;
+		bGameOver = false;
+		
+		print ("=========================");
+		print ("Welcome to Number Wizard!");
+		print ("Please take a moment to think of a 'sceret' number, which I will try to ascertain.");
+		print ("the highest numer in the range is " + RangeTop);
+		print ("the lowest numer in the range is " + RangeBottom);
+		RangeTop ++;
+		NW_Guess();
 	}
 	
-	// Update is called once per frame
 	void Update () {
+       		if (Input.GetKeyDown(KeyCode.UpArrow)) { 
+       			print ("Up Cusror pressed"); 
+       			RangeBottom = CurrentGuess;
+			NW_Guess ();
+     		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) { 
+			print ("Down Cusror pressed"); 
+			RangeTop = CurrentGuess;
+			NW_Guess ();
+			}
+		else if (Input.GetKeyDown(KeyCode.Space)) { 
+			print ("Space bar pressed");
+			bGameOver = true;
+			NW_GameOver();
+			}
+	}
 	
+	void NW_Guess () {
+		// CurrentGuess = (RangeTop + RangeBottom)/ 2;
+		CurrentGuess = Random.Range (RangeBottom, RangeTop);
+		
+		print ("My guess is " + CurrentGuess +".");
+		print ("If you number is higher than my guess tap the Up arrow, if it's lower the Down arrow, and if it's a Match please tap the Space bar.");
+	}
+	
+	void NW_GameOver () {
+		if (bGameOver) {
+			print ("I'll always win! But feel free to play again!");
+			StartNWGame();
+		}
 	}
 }
+
+//lecture 17
