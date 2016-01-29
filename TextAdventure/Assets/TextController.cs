c806396c85bb2990753402f7215f24af23c32c18
JackDraak <jackdraak@gmail.com>
2016-01-29 13:21:43 -0500

    this and that changed...

--- TextAdventure/Assets/TextController.cs
+++ TextAdventure/Assets/TextController.cs
@@ -5,88 +5,133 @@ using System.Collections;
 public class TextController : MonoBehaviour { 
 
 	public Text text;
-	private enum States {Bedroom_1, Bedroom_T, Bedroom_W, Bedroom_A, Bedroom_D, Bedroom_R, Bedroom_S, Bathroom_1};
+	private enum States {Bed, Dresser, Bathroom};
 	private States MyState;
-	  
-
+	private bool bBedDouble = false;
+	private bool bBedThrow = false;
+	private bool bBedWait = false;
+	private bool bBedSceneExit = false;
+	private bool bBedSleep = false;
+	private bool bDresserThinking = false;
+	private int cBedDouble = 0;
+	
 	// Use this for initialization
 	void Start () {
-		MyState = States.Bedroom_1;
-	
+		MyState = States.Bed;	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		print (MyState);
-		
-		if (MyState == States.Bedroom_1) State_Bedroom_1();
-		if (MyState == States.Bedroom_T) State_Bedroom_T();
-		if (MyState == States.Bedroom_W) State_Bedroom_W();
-		if (MyState == States.Bedroom_A) State_Bedroom_A();
-		if (MyState == States.Bedroom_D) State_Bedroom_D();
-		if (MyState == States.Bedroom_R) State_Bedroom_R();
-		if (MyState == States.Bedroom_S) State_Bedroom_S();
-		if (MyState == States.Bathroom_1) State_Bathroom_1();
+		if (MyState == States.Bed) State_Bed();
+		else if (MyState == States.Dresser) State_Dresser();
+		else if (MyState == States.Bathroom) State_Bathroom();
 	}
-	void State_Bedroom_1 () {
-		text.text = "There it goes again. Every stinking day... \n\n" +
+	
+	void State_Bed() {
+		if (!bBedThrow && !bBedWait && !bBedSceneExit && !bBedDouble && !bBedSleep) {
+			text.text = "There it goes again. Every stinking day... \n\n" +
 			"The din from the alarm clock is muffled by the pillows and " +
 			"sheets strewn uncerimonously over you, head included. Ted or " +
-			"Jill must have thrown them over you when your inebriation got " +
-			"the better of you sometime last night. That's probably also the reason " +
+			"Jill must have thrown them over you when your inebriation got the " +
+			"better of you sometime last night. That's probably also the reason " +
 			"you would like to...\n\n" +
 			"<T>hrow a shoe at the alarm clock\n" +
 			"<W>ait it out, muffling your hung-over head with a couple of pillows";
-		if (Input.GetKeyDown(KeyCode.T)) MyState = States.Bedroom_T;
-		if (Input.GetKeyDown(KeyCode.W)) MyState = States.Bedroom_W;
-	}
-	void State_Bedroom_T () {
-		text.text = "YES!!!!\n\n" +
+			if (Input.GetKeyDown(KeyCode.T)) {cBedDouble++; bBedThrow = true;}
+			else if (Input.GetKeyDown(KeyCode.W)) bBedWait = true;
+		}
+		else if (bBedThrow && !bBedWait && !bBedSceneExit && !bBedDouble && !bBedSleep) {
+			text.text = "YES!!!!\n\n" +
 			"It certainly seemed like a good idea at the time, but now that you've " +
 			"missed the alarm clock you also realize that what you chucked was an " +
 			"empty bottle. Of course it exploded against the wall. And, of course, " +
-			"the alarm is growing louder and faster (because You needed the *special* " +
-			"one!) the longer you ignore it. Re-considering your options, you...\n\n" +
+			"the alarm is growing louder and faster the longer you ignore it (because " +
+			"You needed the *special* one!). Re-considering your options, you...\n\n" +
 			"<A>void the broken glass, and go to turn off the alarm clock\n" +
 			"<D>ouble-down and throw a show at the alarm clock";
-		if (Input.GetKeyDown(KeyCode.A)) MyState = States.Bedroom_A;
-		if (Input.GetKeyDown(KeyCode.D)) MyState = States.Bedroom_D;
-	}
-	void State_Bedroom_W () {
-		text.text = "mmmmmHmmm...\n\n" +
+			if (Input.GetKeyDown(KeyCode.A)) { bBedSceneExit = true; MyState = States.Dresser; }
+			else if (Input.GetKeyDown(KeyCode.D)) { cBedDouble++; bBedDouble = true;}	
+		}
+		else if (bBedWait && !bBedThrow && !bBedSceneExit && !bBedDouble && !bBedSleep) {
+			text.text = "mmmmmHmmm...\n\n" +
 			"This isn't getting you anywhere. The alarm is getting louder and more " +
 			"frantic. You grudingly ease your self from your nest and trip over " +
 			"yourself in the darkness to finally bring about a welcomed silence. " +
-			"You can't recall teh last time you partied that hard, but then again " +
+			"You can't recall the last time you partied that hard, but then again " +
 			"It isn't every day three friends find the discovery of the century...\n\n" +
 			"<R>ight! Forgot about that! Time to do some thinking!\n" +
-			"<S>hake it off, your memory of yesterday must be off. Time for a hot shower";
-		if (Input.GetKeyDown(KeyCode.R)) MyState = States.Bedroom_R;
-		if (Input.GetKeyDown(KeyCode.S)) MyState = States.Bathroom_1;
+			"<S>hake it off, your memory of yesterday must be off. Head back to bed.";
+			if (Input.GetKeyDown(KeyCode.R)) {bDresserThinking = true; MyState = States.Dresser; }
+			else if (Input.GetKeyDown(KeyCode.S)) bBedSleep = true;
+		}
+		else if (bBedDouble && !bBedWait && !bBedSceneExit && !bBedSleep) {
+			text.text = "WHY!!!!\n\n" +
+			"A part of you is rather uncertain why you'd go and do this again. " +
+			"Another part of you wonders how much glass " + cBedDouble + " bottles " +
+			"have left between you and the alarm clock on the dresser at the other " +
+			"side of the room. Yet another part of you decides to actually, you know, " +
+			"do something, like...\n\n" +
+			"<A>void the broken glass, and go to turn off the alarm clock\n" +
+			"<T>riple-down? and throw a show at the alarm clock";
+			if (Input.GetKeyDown(KeyCode.A)) { bBedSceneExit = true; MyState = States.Dresser; }
+			else if (Input.GetKeyDown(KeyCode.T)) { cBedDouble++; bBedDouble = true; }
+		}
+		else if (bBedSleep) {
+			text.text = "Good Night\n\n" +
+			"Hopefully you're less discombobulated the next timne you wake-up...\n\n" +
+			"<R>eset game state\n" +
+			"<S>oft-restart";
+			if (Input.GetKeyDown(KeyCode.S)) { bBedSleep=false; }
+			else if (Input.GetKeyDown(KeyCode.R)) { 
+				bBedDouble = false;
+				bBedThrow = false;
+				bBedWait = false;
+				bBedSceneExit = false;
+				bBedSleep = false;
+				bDresserThinking = false;
+				cBedDouble = 0;
+				bBedSceneExit = false; 
+				MyState = States.Bed; 
+			}
+		}
 	}
-	void State_Bedroom_A () {
-		text.text = "Bedroom_A\n\n" +
+	
+	void State_Dresser () {
+		if (!bDresserThinking) {
+			text.text = "Dresser, not thinking, per se\nPress R to reset game state";
+			if (Input.GetKeyDown(KeyCode.R)) { 
+				bBedDouble = false;
+				bBedThrow = false;
+				bBedWait = false;
+				bBedSceneExit = false;
+				bBedSleep = false;
+				bDresserThinking = false;
+				cBedDouble = 0;
+				bBedSceneExit = false; 
+				MyState = States.Bed; 
+			}
+		}
+		else if (bDresserThinking) {
+			text.text = "Dresser, thinking.\nPress R to reset game state";
+			if (Input.GetKeyDown(KeyCode.R)) { 
+				bBedDouble = false;
+				bBedThrow = false;
+				bBedWait = false;
+				bBedSceneExit = false;
+				bBedSleep = false;
+				cBedDouble = 0;
+				bBedSceneExit = false; 
+				bDresserThinking = false;
+				MyState = States.Bed; // will this execute?
+			}
+		}
+	}
+	
+	void State_Bathroom () {
+		text.text = "Bathroom\n\n" +
 			"space to restart";
-		if (Input.GetKeyDown(KeyCode.Space)) MyState = States.Bedroom_1;
+		if (Input.GetKeyDown(KeyCode.Space)) MyState = States.Bed;
 	}
-	void State_Bathroom_1 () {
-		text.text = "Bathroom_1\n\n" +
-			"space to restart";
-		if (Input.GetKeyDown(KeyCode.Space)) MyState = States.Bedroom_1;
-	}
-	void State_Bedroom_R () {
-		text.text = "Bedroom_R\n\n" +
-			"space to restart";
-		if (Input.GetKeyDown(KeyCode.Space)) MyState = States.Bedroom_1;
-	}
-	void State_Bedroom_S () {
-		text.text = "Bedroom_R\n\n" +
-			"space to restart";
-		if (Input.GetKeyDown(KeyCode.Space)) MyState = States.Bedroom_1;
-	}
-	void State_Bedroom_D () {
-		text.text = "Bedroom_D\n\n" +
-			"space to restart";
-		if (Input.GetKeyDown(KeyCode.Space)) MyState = States.Bedroom_1;
-	}
+
 }
