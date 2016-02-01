48742f76edd2f248c74cf69059d4036fa89d10fc
JackDraak <jackdraak@gmail.com>
2016-01-31 21:56:12 -0500

    breakout setup

---
+++ Breakout/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string LevelName){
+	print ("Level Manager : Load Level : " + LevelName );
+	Application.LoadLevel(LevelName);
+	}
+}
---
+++ Breakout/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	static MusicManager instance = null;
+	
+	void Awake () {
+		Debug.Log ("This instance: Awake : MusicManager : " + this.GetInstanceID());
+		if (instance != null) {
+			Destroy (gameObject);
+			Debug.Log ("destroy duplicate MusicManager -executed-");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	void Start () {
+		Debug.Log ("This instance : Start : MusicManager : " + this.GetInstanceID());
+	}
+}
