9126058856411e121c79c5caa8dd0f74866a1aa5
JackDraak <jackdraak@gmail.com>
2016-02-09 17:10:11 -0500

    OCD tidying

--- _BlockBreaker/Assets/Scripts/Paddle.cs
+++ _BlockBreaker/Assets/Scripts/Paddle.cs
@@ -10,33 +10,34 @@ public class Paddle : MonoBehaviour {
 	private Vector3 ballPos;
 	private GameObject startNote;
 	private LevelManager levelManager;
-
-	void EasyFlip ()		{ if (easy) this.transform.localScale = new Vector2(2.5f,1); else this.transform.localScale = new Vector2(1.5f,1); }
-	void EasyPlayOff ()		{ easy = false; EasySync(); }
-	void EasyPlayOn ()		{ easy = true; EasySync(); }
-	void EasySync ()		{ PlayerPrefsManager.SetEasy (easy); EasyFlip(); }
-	void ToggleAuto () 		{ autoplay = !autoplay; PlayerPrefsManager.SetAutoplay (autoplay); }
-	void ToggleEasy ()		{ easy = !easy; EasySync(); }
 	
-	void Start () {
-		levelManager = GameObject.FindObjectOfType<LevelManager>();	if (!levelManager) Debug.LogError (this + ": unable to attach to LevelManager");
-		if (GameObject.FindGameObjectWithTag ("StartNote")) startNote = GameObject.FindGameObjectWithTag ("StartNote");
-		ball = GameObject.FindObjectOfType<Ball>();	if (!ball) Debug.LogError (this + ": unable to attach to Ball");
-		autoplay = PlayerPrefsManager.GetAutoplay ();
-		easy = PlayerPrefsManager.GetEasy ();
-		EasyFlip();
+	void BeginPlay () {
+		levelManager.HasStartedTrue();
+		if (startNote) startNote.SetActive (false);
+		ball.GetComponent<Rigidbody2D>().velocity = new Vector2 (Random.Range(-8f, 8f), Random.Range(8f, 10f));
 	}
 
-	// TODO this really ought to be in LevelManager or something, eh?
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.A)) {ToggleAuto();}
-		if (Input.GetKeyDown(KeyCode.E)) {ToggleEasy();}
-	}
-
+	void EasyFlip ()	{ if (easy) this.transform.localScale = new Vector2(2.5f,1); else this.transform.localScale = new Vector2(1.5f,1); }
+	void EasyPlayOff ()	{ easy = false; EasySync(); }
+	void EasyPlayOn ()	{ easy = true; EasySync(); }
+	void EasySync ()	{ PlayerPrefsManager.SetEasy (easy); EasyFlip(); }
+	
 	void FixedUpdate () {
 		PaddleMotion();
 	}
 
+	void OnTriggerEnter2D (Collider2D trigger) { 
+		AudioSource.PlayClipAtPoint (paddle, transform.position); // optional 3rd float value for volume
+	}
+	
+	float PaddleClamp (float xPos) {
+		if (easy) {
+			return Mathf.Clamp((xPos), 1.916f, 14.086f); // easy paddle
+		} else {
+			return Mathf.Clamp((xPos), 1.390f, 14.615f); // normal paddle
+		}
+	}
+	
 	void PaddleMotion () {
 		Vector3 paddlePos = new Vector3 (8f, this.transform.position.y, 0f);
 		if (!autoplay) 	{
@@ -56,21 +57,21 @@ public class Paddle : MonoBehaviour {
 		this.transform.position = paddlePos;
 	}
 
-	float PaddleClamp (float xPos) {
-		if (easy) {
-			return Mathf.Clamp((xPos), 1.916f, 14.086f); // easy paddle
-		} else {
-			return Mathf.Clamp((xPos), 1.390f, 14.615f); // normal paddle
-		}
-	}
-
-	void BeginPlay () {
-		levelManager.HasStartedTrue();
-		if (startNote) startNote.SetActive (false);
-		ball.GetComponent<Rigidbody2D>().velocity = new Vector2 (Random.Range(-8f, 8f), Random.Range(8f, 10f));
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();	if (!levelManager) Debug.LogError (this + ": unable to attach to LevelManager");
+		if (GameObject.FindGameObjectWithTag ("StartNote")) startNote = GameObject.FindGameObjectWithTag ("StartNote");
+		ball = GameObject.FindObjectOfType<Ball>();	if (!ball) Debug.LogError (this + ": unable to attach to Ball");
+		autoplay = PlayerPrefsManager.GetAutoplay ();
+		easy = PlayerPrefsManager.GetEasy ();
+		EasyFlip();
 	}
 	
-	void OnTriggerEnter2D (Collider2D trigger) { 
-		AudioSource.PlayClipAtPoint (paddle, transform.position); // optional 3rd float value for volume
+	void ToggleAuto () { autoplay = !autoplay; PlayerPrefsManager.SetAutoplay (autoplay); }
+	void ToggleEasy () { easy = !easy; EasySync(); }
+
+	// TODO this really ought to be in LevelManager or something, eh? y'think? meh... good enough for now....
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {ToggleAuto();}
+		if (Input.GetKeyDown(KeyCode.E)) {ToggleEasy();}
 	}
 }
--- _BlockBreaker/Assets/Scripts/PlayerPrefsManager.cs
+++ _BlockBreaker/Assets/Scripts/PlayerPrefsManager.cs
@@ -7,65 +7,43 @@ public class PlayerPrefsManager : MonoBehaviour {
 	const string AWARD_KEY = 		"award";
 	const string EASYMODE_KEY = 	"easymode";
 	const string FIREBALLS_KEY = 	"fireballs";
-	const string MASTER_VOL_KEY = 	"master_volume"; // TODO work in progress
+	const string MASTER_VOL_KEY = 	"master_volume"; // TODO work in progress, not in options_scene
 	const string SPEED_KEY = 		"speed";
 	const string TOPSCORE_KEY = 	"topscore";
 	const string TRAILS_KEY = 		"trails";
-	
-	public static int GetAward () 					{ return PlayerPrefs.GetInt (AWARD_KEY); }
-	public static void SetAward (int award) 		{ PlayerPrefs.SetInt (AWARD_KEY, award); }
-	public static float GetMasterVolume () 			{ return PlayerPrefs.GetFloat (MASTER_VOL_KEY); }
-	public static float GetSpeed () 				{ return PlayerPrefs.GetFloat (SPEED_KEY); }
-	public static void SetSpeed (float yorn) 		{ PlayerPrefs.SetFloat (SPEED_KEY, yorn); }
-	public static float GetTopscore () 				{ return PlayerPrefs.GetFloat (TOPSCORE_KEY); }
-	public static void SetTopscore (float yorn) 	{ PlayerPrefs.SetFloat (TOPSCORE_KEY, yorn); }
-
-	// Fireballs
-	public static void SetFireBalls (bool set) {
-		if (set) PlayerPrefs.SetInt (FIREBALLS_KEY, 1);
-		else  PlayerPrefs.SetInt (FIREBALLS_KEY, 0);
-	}
-	public static bool GetFireBalls () {
-		if (PlayerPrefs.GetInt (FIREBALLS_KEY) == 1) return true;
-		else return false;
-	}
-
-	// TrippyTrails
-	public static void SetTrails (bool set) {
-		if (set) PlayerPrefs.SetInt (TRAILS_KEY, 1);
-		else PlayerPrefs.SetInt (TRAILS_KEY, 0);
-	}
-	public static bool GetTrails () {
-		if (PlayerPrefs.GetInt (TRAILS_KEY) == 1) return true;
-		else return false;
-	}
-
-	// Easy mode
-	public static void SetEasy (bool set) {
-		if (set) PlayerPrefs.SetInt (EASYMODE_KEY, 1);
-		else PlayerPrefs.SetInt (EASYMODE_KEY, 0);
-	}
-	public static bool GetEasy () {
-		if (PlayerPrefs.GetInt (EASYMODE_KEY) == 1) return true;
-		else return false; 
-	}
 
 	// Autoplay
-	public static void SetAutoplay (bool set) {
-		if (set) PlayerPrefs.SetInt (AUTOPLAY_KEY, 1);
-		else PlayerPrefs.SetInt (AUTOPLAY_KEY, 0);
-	}
-	public static bool GetAutoplay () {
-		if (PlayerPrefs.GetInt (AUTOPLAY_KEY) == 1) return true;
-		else return false;
+	public static bool GetAutoplay () { if (PlayerPrefs.GetInt (AUTOPLAY_KEY) == 1) return true; else return false; }
+	public static void SetAutoplay (bool set) { if (set) PlayerPrefs.SetInt (AUTOPLAY_KEY, 1); else PlayerPrefs.SetInt (AUTOPLAY_KEY, 0); }
+	
+	// Award
+	public static int GetAward () { return PlayerPrefs.GetInt (AWARD_KEY); }
+	public static void SetAward (int award) { PlayerPrefs.SetInt (AWARD_KEY, award); }
+	
+	// Easy mode
+	public static bool GetEasy () { if (PlayerPrefs.GetInt (EASYMODE_KEY) == 1) return true; else return false; }
+	public static void SetEasy (bool set) { if (set) PlayerPrefs.SetInt (EASYMODE_KEY, 1); else PlayerPrefs.SetInt (EASYMODE_KEY, 0); }
+	
+	// Fireballs
+	public static bool GetFireBalls () { if (PlayerPrefs.GetInt (FIREBALLS_KEY) == 1) return true; else return false; }
+	public static void SetFireBalls (bool set) { if (set) PlayerPrefs.SetInt (FIREBALLS_KEY, 1); else PlayerPrefs.SetInt (FIREBALLS_KEY, 0); }
+
+	// MasterVolume
+	public static float GetMasterVolume () { return PlayerPrefs.GetFloat (MASTER_VOL_KEY); }
+	public static void SetMasterVolume (float volume) { 
+		if (volume >= 0f && volume <= 1f) { PlayerPrefs.SetFloat (MASTER_VOL_KEY, volume); } 
+		else {Debug.LogError ("Master volume out of range"); }
 	}
 
-	// Volume // TODO not fully implemented
-	public static void SetMasterVolume (float volume) {
-		if (volume >= 0f && volume <= 1f) {
-			PlayerPrefs.SetFloat (MASTER_VOL_KEY, volume);
-		} else {
-			Debug.LogError ("Master volume out of range");
-		}
-	}
+	// SpeedLimit
+	public static float GetSpeed () { return PlayerPrefs.GetFloat (SPEED_KEY); }
+	public static void SetSpeed (float yorn) { PlayerPrefs.SetFloat (SPEED_KEY, yorn); }
+	
+	// Topscore
+	public static float GetTopscore () { return PlayerPrefs.GetFloat (TOPSCORE_KEY); }
+	public static void SetTopscore (float yorn) { PlayerPrefs.SetFloat (TOPSCORE_KEY, yorn); }
+	
+	// TrippyTrails
+	public static bool GetTrails () { if (PlayerPrefs.GetInt (TRAILS_KEY) == 1) return true; else return false; }
+	public static void SetTrails (bool set) { if (set) PlayerPrefs.SetInt (TRAILS_KEY, 1); else PlayerPrefs.SetInt (TRAILS_KEY, 0); }
 }
