7f9512cc5c42d30ff6b6af3dd1fa5e2800b8ebd9
JackDraak <jackdraak@gmail.com>
2016-02-09 13:15:39 -0500

    tidy 1

--- _BlockBreaker/Assets/Scripts/Paddle.cs
+++ _BlockBreaker/Assets/Scripts/Paddle.cs
@@ -11,16 +11,46 @@ public class Paddle : MonoBehaviour {
 	private LevelManager levelManager;
 	
 	void Start () {
-	// TODO re-enable at some point for release?
-
-	//	Cursor.visible = false; 
-		levelManager = GameObject.FindObjectOfType<LevelManager>();
-		if (!levelManager) Debug.LogError (this + ": unable to attach to LevelManager");
+		//	Cursor.visible = false; // TODO re-enable at some point for release?
+		levelManager = GameObject.FindObjectOfType<LevelManager>();	if (!levelManager) Debug.LogError (this + ": unable to attach to LevelManager");
 		if (GameObject.FindGameObjectWithTag ("StartNote")) startNote = GameObject.FindGameObjectWithTag ("StartNote");
-		ball = GameObject.FindObjectOfType<Ball>();
+		ball = GameObject.FindObjectOfType<Ball>();	if (!ball) Debug.LogError (this + ": unable to attach to Ball");
 		autoplay = PlayerPrefsManager.GetAutoplay ();
 		easy = PlayerPrefsManager.GetEasy ();
 		EasyFlip();
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.A)) {ToggleAuto();}
+		if (Input.GetKeyDown(KeyCode.E)) {ToggleEasy();}
+		PaddleMotion();
+	}
+
+	void PaddleMotion () {
+		Vector3 paddlePos = new Vector3 (8f, this.transform.position.y, 0f);
+		if (!autoplay) 	{
+			float mousePosInBlocks = (Input.mousePosition.x / Screen.width * 16);
+			if (easy) {
+				paddlePos.x = Mathf.Clamp((mousePosInBlocks), 1.916f, 14.086f); // easy paddle
+			} else {
+				paddlePos.x = Mathf.Clamp((mousePosInBlocks), 1.390f, 14.615f); // normal paddle
+			}
+		} 
+		if (autoplay) 	{
+			if (!levelManager.HasStartedReturn() && !begun) {
+				begun = true;
+				Invoke ("BeginPlay", 2);
+			}
+			if (levelManager.HasStartedReturn()) {
+				ballPos = ball.transform.position;
+				if (easy) {
+					paddlePos.x = Mathf.Clamp((ballPos.x), 1.916f, 14.086f); // easy paddle
+				} else {
+					paddlePos.x = Mathf.Clamp((ballPos.x), 1.390f, 14.615f); // normal paddle
+				}
+			}
+		}
+		this.transform.position = paddlePos;
 	}
 
 	void ToggleAuto () {
@@ -50,41 +80,11 @@ public class Paddle : MonoBehaviour {
 		if (easy) this.transform.localScale = new Vector2(2.5f,1);
 		else this.transform.localScale = new Vector2(1.5f,1);
 	}
-	
-	void FixedUpdate () {
-		if (Input.GetKeyDown(KeyCode.A)) {ToggleAuto();}
-		if (Input.GetKeyDown(KeyCode.E)) {ToggleEasy();}
 
-		Vector3 paddlePos = new Vector3 (8f, this.transform.position.y, 0f);
-			if (!autoplay) 	{
-				float mousePosInBlocks = (Input.mousePosition.x / Screen.width * 16);
-				if (easy) {
-					paddlePos.x = Mathf.Clamp((mousePosInBlocks), 1.916f, 14.086f); // easy paddle
-				} else {
-					paddlePos.x = Mathf.Clamp((mousePosInBlocks), 1.390f, 14.615f); // normal paddle
-				}
-			} 
-			if (autoplay) 	{
-				if (!levelManager.HasStartedReturn() && !begun) {
-					begun = true;
-					Invoke ("BeginPlay", 2);
-				}
-				if (levelManager.HasStartedReturn()) {
-					ballPos = ball.transform.position;
-					if (easy) {
-						paddlePos.x = Mathf.Clamp((ballPos.x), 1.916f, 14.086f); // easy paddle
-					} else {
-					paddlePos.x = Mathf.Clamp((ballPos.x), 1.390f, 14.615f); // normal paddle
-					}
-				}
-			}
-			this.transform.position = paddlePos;
-	}
-	
 	void BeginPlay () {
 		levelManager.HasStartedTrue();
 		if (startNote) startNote.SetActive (false);
-		ball.GetComponent<Rigidbody2D>().velocity = new Vector2 (Random.Range(-12f, 12f), Random.Range(8f, 10f));
+		ball.GetComponent<Rigidbody2D>().velocity = new Vector2 (Random.Range(-8f, 8f), Random.Range(8f, 10f));
 	}
 	
 	void OnTriggerEnter2D (Collider2D trigger) { 
