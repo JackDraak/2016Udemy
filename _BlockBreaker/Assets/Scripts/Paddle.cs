4ba57e3203a3e170bfaeb3347b068b96c126869d
JackDraak <jackdraak@gmail.com>
2016-02-10 00:33:13 -0500

    tidy paddle

--- _BlockBreaker/Assets/Scripts/Paddle.cs
+++ _BlockBreaker/Assets/Scripts/Paddle.cs
@@ -13,32 +13,26 @@ public class Paddle : MonoBehaviour {
 	private GameObject startNote;
 	private LevelManager levelManager;
   
-
 	void BeginPlay () {
 		levelManager.HasStartedTrue();
 		if (startNote) startNote.SetActive (false);
 		ball.GetComponent<Rigidbody2D>().velocity = new Vector2 (Random.Range(-8f, 8f), Random.Range(8f, 10f));
 	}
 
-	void EasyFlip ()	{ if (easy) this.transform.localScale = new Vector2(2.5f,1); else this.transform.localScale = new Vector2(1.5f,1); }
-	void EasyPlayOff ()	{ easy = false; EasySync(); }
-	void EasyPlayOn ()	{ easy = true; EasySync(); }
-	void EasySync ()	{ PlayerPrefsManager.SetEasy (easy); EasyFlip(); }
-	
-	void FixedUpdate () {
-		PaddleMotion();
-	}
+	void EasyFlip () { if (easy) this.transform.localScale = new Vector2(2.5f,1); else this.transform.localScale = new Vector2(1.5f,1); }
+	void EasyPlayOff () { easy = false; EasySync(); }
+	void EasyPlayOn () { easy = true; EasySync(); }
+	void EasySync () { PlayerPrefsManager.SetEasy (easy); EasyFlip(); }
+
+	void FixedUpdate () { PaddleMotion(); }
 
 	void OnTriggerEnter2D (Collider2D trigger) { 
 		AudioSource.PlayClipAtPoint (paddle, transform.position); // optional 3rd float value for volume
 	}
 	
 	float PaddleClamp (float xPos) {
-		if (easy) {
-			return Mathf.Clamp((xPos), 1.916f, 14.086f); // easy paddle
-		} else {
-			return Mathf.Clamp((xPos), 1.390f, 14.615f); // normal paddle
-		}
+		if (easy) { return Mathf.Clamp((xPos), 1.916f, 14.086f); } // easy paddle
+		else { return Mathf.Clamp((xPos), 1.390f, 14.615f); } // normal paddle
 	}
 	
 	void PaddleMotion () {
@@ -71,21 +65,15 @@ public class Paddle : MonoBehaviour {
 
 	void TestDriftDirection (float span) {
  		if (span > driftThat || span < driftThis ) {
- 			driftThat = Random.Range (0.3f, 0.8f);
- 			driftThis = -driftThat;
+ 			driftThat = Random.Range (0.3f, 0.8f); driftThis = -driftThat;
  			driftDirection = !driftDirection;
  		}
  	}
  
 	void AutoMove (float jitter) {
-		float ballXPosition = ball.transform.position.x;
-		float window = ballXPosition + jitter; 
-		float paddleXPosition = PaddleClamp (window);
-		Vector3 paddlePosition = new Vector3 (paddleXPosition, this.transform.position.y, 0f);
+		Vector3 paddlePosition = new Vector3 (PaddleClamp (ball.transform.position.x + jitter), this.transform.position.y, 0f);
 		this.transform.position = paddlePosition;
-//		Debug.Log (this.transform.position.x - ball.transform.position.x );
 	}
-
 
 	void Start () {
 		levelManager = GameObject.FindObjectOfType<LevelManager>();	if (!levelManager) Debug.LogError (this + ": unable to attach to LevelManager");
@@ -99,7 +87,6 @@ public class Paddle : MonoBehaviour {
 	void ToggleAuto () { autoplay = !autoplay; PlayerPrefsManager.SetAutoplay (autoplay); }
 	void ToggleEasy () { easy = !easy; EasySync(); }
 
-	// TODO this really ought to be in LevelManager or something, eh? y'think? meh... good enough for now....
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.A)) {ToggleAuto();}
 		if (Input.GetKeyDown(KeyCode.P)) {ToggleEasy();}
