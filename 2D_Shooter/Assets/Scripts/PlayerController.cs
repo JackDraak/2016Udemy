134db6ea04507b4fef15b3fd596fd639a6e93a84
JackDraak <jackdraak@gmail.com>
2016-02-12 06:22:54 -0500

    tidy

--- 2D_Shooter/Assets/Scripts/PlayerController.cs
+++ 2D_Shooter/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
 	public GameObject player;
 
 	private float acceleration;
@@ -11,38 +12,36 @@ public class PlayerController : MonoBehaviour {
 	private float maxAcceleration;
 	private float maxSpeed;
 	private Vector3 nextPos;
-	private Vector3 playerPos;
-
-
-	void SetNextPos () {
-		playerPos = player.transform.position;
-		if (right) nextPos = new Vector3(SetXClamps(playerPos.x + lateralVelocity), playerPos.y, playerPos.z);
-		else  nextPos = new Vector3(SetXClamps(playerPos.x - lateralVelocity), playerPos.y, playerPos.z);
-		player.transform.position = nextPos;
-		Debug.Log ("Accel:" + acceleration + ", LVelocity:" + lateralVelocity);
-	}
-
-	float SetXClamps(float position) {
-		return Mathf.Clamp(position, 0.55f, 15.41f);
-	}
-
-	void SetVelocity () {
-		if (acceleration < maxAcceleration) acceleration = acceleration + baseAcceleration;
-		if (lateralVelocity < maxSpeed) lateralVelocity = lateralVelocity + acceleration;
-	}
 
 	void SetLeftward () {
 		if (right) { right = !right; lateralVelocity = 0f; acceleration = 0f;}
+//		transform.rotation.y = 17f;
 		SetVelocity();
 		SetNextPos();
 	}
 
 	void SetRightward() {
 		if (!right) { right = !right; lateralVelocity = 0f; acceleration = 0f;}
+//		transform.rotation.y = -17f;
 		SetVelocity();
 		SetNextPos();
 	}
 
+	void SetNextPos () {
+		if (right) transform.position = new Vector3(SetXClamps(transform.position.x + lateralVelocity), transform.position.y, transform.position.z);
+		else  transform.position = new Vector3(SetXClamps(transform.position.x - lateralVelocity), transform.position.y, transform.position.z);
+		//		Debug.Log ("Accel:" + acceleration + ", LVelocity:" + lateralVelocity);
+	}
+	
+	void SetVelocity () {
+		if (acceleration < maxAcceleration) acceleration = acceleration + baseAcceleration;
+		if (lateralVelocity < maxSpeed) lateralVelocity = lateralVelocity + acceleration;
+	}
+	
+	float SetXClamps(float position) {
+		return Mathf.Clamp(position, 0.55f, 15.41f);
+	}
+	
 	void Start () {
 		acceleration = 0f;
 		baseAcceleration = 0.02f;
@@ -54,6 +53,7 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		if (acceleration > 0.001f) acceleration =- 0.01f; 
 		if (Input.GetKey(KeyCode.LeftArrow)) SetLeftward();
-		if (Input.GetKey(KeyCode.RightArrow)) SetRightward();
+		else if (Input.GetKey(KeyCode.RightArrow)) SetRightward();
+	//	else transform.rotation.y = 0f;
 	}
 }
