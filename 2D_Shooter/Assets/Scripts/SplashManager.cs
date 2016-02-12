ca3460471f25e7ce5055a28614a9d910d72385ba
JackDraak <jackdraak@gmail.com>
2016-02-12 03:16:23 -0500

    imported menu system
    Signed-off-by: JackDraak <jackdraak@gmail.com>

---
+++ 2D_Shooter/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashManager : MonoBehaviour {
+
+	public float autoLoadNextLevelDelay = 2;
+
+	private OptionsController optionsController;
+	
+	void Start () {
+		Cursor.visible = false;
+		optionsController = GameObject.FindObjectOfType<OptionsController>(); if (!optionsController) Debug.LogError (this + ":ERROR: unable to attach to OptionsController to reset options to defaults");
+		optionsController.SetDefaults();
+		optionsController.Save ();
+		Invoke("LoadNextLevel", autoLoadNextLevelDelay);
+	}
+	
+	public void LoadNextLevel() {
+		Cursor.visible = true;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+	}
+}
