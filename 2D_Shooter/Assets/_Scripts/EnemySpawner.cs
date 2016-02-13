7d2dba9047e8494144e62bf1ff21e8230add5290
JackDraak <jackdraak@gmail.com>
2016-02-12 21:27:58 -0500

    messy snapshot, just because it needs to be done

--- 2D_Shooter/Assets/_Scripts/EnemySpawner.cs
+++ 2D_Shooter/Assets/_Scripts/EnemySpawner.cs
@@ -4,11 +4,31 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab;
+//	public GameObject positionPrefab;
 
-
-	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab, new Vector3 (1, 1, 0), Quaternion.identity) as GameObject;
-		enemy.transform.parent = this.transform;
+//		SpawnPosition (-1f, -1f);
+//		SpawnPosition (-1f, 1f);
+//		SpawnPosition (1f, -1f);
+//		SpawnPosition (1f, 1f);
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
 	}
+
+	void OnDrawGizmos () {
+		Gizmos.DrawWireCube(transform.position, new Vector3 (4,4,4));
+	}
+
+//	void SpawnEnemy () {
+//		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+//		enemy.transform.parent = transform;
+//	}
+
+//	void SpawnPosition (float PosX, float PosY) {
+//		GameObject position = Instantiate(positionPrefab, new Vector3(PosX, PosY, 0), Quaternion.identity) as GameObject;
+//		position.transform.parent = transform;
+//	}
+
 }
