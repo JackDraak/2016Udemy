64371ff86e48b30728273b820d4b0270db2a3b4d
JackDraak <jackdraak@gmail.com>
2016-02-13 17:34:25 -0500

    trying to get to formation to decelerate before reversing

--- 2D_Shooter/Assets/_Scripts/EnemyController.cs
+++ 2D_Shooter/Assets/_Scripts/EnemyController.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
-	private bool right, shoot;
+	private bool decelerate, right, shoot;
 	private float xMax, xMin;
-	private float padding = 0.6f;
+	private float padding = 3.2f;
 	private float acceleration;
 	private float baseAcceleration;
 	private float lateralVelocity;
@@ -13,12 +13,15 @@ public class EnemyController : MonoBehaviour {
 	private float maxSpeed;
 	private Vector3 tempPos;
 
+	public float reverseBuffer =1f;
+	public float reverseSquelch = 1f;
+
 	void Start () { 
 		acceleration = 0f;
-		baseAcceleration = 0.025f;
+		baseAcceleration = 0.00010f;
 		lateralVelocity = 0f;
-		maxAcceleration = 0.7f;
-		maxSpeed = 1.4f;
+		maxAcceleration = 0.003f;
+		maxSpeed = 0.21f;
 		right = true;
 		shoot = false;
 		SetMinMaxX();
@@ -45,14 +48,26 @@ public class EnemyController : MonoBehaviour {
 	}
 
 	void SetNextPos () {
-		tempPos = transform.position;
+		BoundaryTestAndFlip ();
+		SetVelocity();
 		if (right) transform.position = new Vector3(SetXClamps(tempPos.x + lateralVelocity), tempPos.y, tempPos.z);
 		else  transform.position = new Vector3(SetXClamps(tempPos.x - lateralVelocity), tempPos.y, tempPos.z);
+	}
+
+	void BoundaryTestAndFlip () {
+		tempPos = transform.position;
+		if (tempPos.x <= xMin || tempPos.x >= xMax) {
+			right = !right;
+			lateralVelocity = 0.00010f;
+			acceleration = baseAcceleration;
+		}
 	}
 	
 	void SetVelocity () {
 		if (acceleration < maxAcceleration) acceleration = acceleration + baseAcceleration;
 		if (lateralVelocity < maxSpeed) lateralVelocity = lateralVelocity + acceleration;
+		if (tempPos.x < xMin - reverseBuffer || tempPos.x > xMax + reverseBuffer)  lateralVelocity = lateralVelocity / reverseSquelch;
+		Debug.Log (lateralVelocity);
 	}
 	
 	float SetXClamps (float position) {
@@ -60,12 +75,7 @@ public class EnemyController : MonoBehaviour {
 	}
 	
 	void Update () {
-		// test direction
-		if (right) {
-		// move right (test boundary, toggle direction if needed, move)
-		} else {
-			// move left (test boundary, toggle direction if needed, move)
-		}
+		SetNextPos();
 	}
 
 	void ShootToggle () {
