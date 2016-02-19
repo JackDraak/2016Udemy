f806ab3b8ad484b2d156c27cb084f968ea3d1734
JackDraak <jackdraak@gmail.com>
2016-02-19 14:16:05 -0500

    pre-work

---
+++ 2D_Shooter/Assets/Sky_Scene/_Scripts/SmokeMachine.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SmokeMachine : MonoBehaviour {
+	void Start () {
+		ParticleSystem smoke = GetComponent<ParticleSystem>();
+		smoke.Emit(10);
+		Destroy(gameObject, 1.5f);
+	}
+}
