f357ee337b2793a39a025599e4e175be372a4cd9
JackDraak <jackdraak@gmail.com>
2016-02-19 21:26:59 -0500

    tidying IDE

--- 2D_Shooter/Assets/Sky_Scene/_Scripts/Shredder.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-public class Shredder : MonoBehaviour {
-	void OnTriggerEnter2D (Collider2D trash) { Destroy(trash.gameObject); }
-}
--- 2D_Shooter/Assets/Sky_Scene/_Scripts/SmokeMachine.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-public class SmokeMachine : MonoBehaviour {
-	void Start () {
-		ParticleSystem smoke = GetComponent<ParticleSystem>();
-		smoke.Emit(10);
-		Destroy(gameObject, 1.5f);
-	}
-}
