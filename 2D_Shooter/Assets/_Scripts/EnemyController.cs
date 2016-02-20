93e3fd2a6d13dc9e31669de8c8b8d6453c1e70a7
JackDraak <jackdraak@gmail.com>
2016-02-19 21:30:41 -0500

    IDE tidy

---
+++ 2D_Shooter/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	// adjust/set in inspector!
+	public GameObject bomb;
+	public AudioClip bombSound;
+	public AudioClip damage;
+	public AudioClip scuttle;
+	public GameObject puffMachine;
+
+	private bool armed, dearmed;
+	private float bombSpeed, chance, fireDelay, fireTime, hitPoints, maxHealth;
+	private Color currentColor;
+	private LevelManager levelManager;
+	private SpriteRenderer myRenderer;
+	private Vector3 myScale;
+	
+	public void Disarm () { dearmed = true; }
+	public void Rearm () { dearmed = false; }
+
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>(); 
+			if (!levelManager) Debug.LogError ("LEVEL_MANAGER_FAIL");
+		myRenderer = GetComponent<SpriteRenderer>();
+			if (!myRenderer) Debug.LogError ("FAIL renderer");
+
+		armed = true;
+		bombSpeed = 6f;
+		dearmed = false;
+		fireDelay = 1.4f;
+		fireTime = Time.time;
+		maxHealth = 111f;
+
+		hitPoints = maxHealth;
+	}
+
+	void Update () {
+		// fire control
+		if (!dearmed) {
+			if (!armed) {
+				InvokeRepeating ("DropBomb", fireDelay, fireDelay);
+				armed = !armed;
+			}
+		}
+
+		// fire control reset?
+		chance = Random.Range (1, 100);
+		if (chance > 49 && chance < 51) armed = !armed;
+
+		// damage haptics -- desire: colour 1, 1, 1, 1 at full health slipping to 1, 0, 0, 1 at death
+		Vector4 priorColour = currentColor;
+		float colourDelta = maxHealth / hitPoints;
+		currentColor = new Vector4 (1, 1/colourDelta, 1/colourDelta, 1f);
+		if (!Mathf.Approximately(priorColour.y, currentColor.g))  myRenderer.color = currentColor;
+	}
+	
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.tag == "PlayerProjectile") {
+			TakeDamage();
+			if (collider.gameObject) Destroy (collider.gameObject, 0.01f);
+		}
+	}
+	
+	void DropBomb () {
+		if (fireTime + fireDelay <= Time.time) {
+			GameObject discharge = Instantiate(bomb, transform.position, Quaternion.identity) as GameObject;
+			discharge.GetComponent<Rigidbody2D>().velocity += Vector2.down * bombSpeed;
+			AudioSource.PlayClipAtPoint (bombSound, transform.position);
+			fireTime = Time.time + Random.Range(0.0f, 4.0f);
+		}
+	}
+	
+	void TakeDamage () {
+		hitPoints = (hitPoints * 0.93f) - 23f;
+		AudioSource.PlayClipAtPoint (damage, transform.position);
+		GameObject trash = Instantiate(puffMachine, transform.position, Quaternion.identity) as GameObject;
+		Destroy (trash, 2);
+		if (hitPoints <= 0f) ScoreAndDestroy();
+	}
+
+	void ScoreAndDestroy () {
+		// TODO typical time to randomly "drop a bonus"
+		levelManager = GameObject.FindObjectOfType<LevelManager>(); // why the heck do I need this here to prevent exception faults?
+			if (!levelManager) Debug.LogError ("LEVEL_MANAGER_FAIL");
+		levelManager.ChangeScore(10f); // TODO multiply by wace#, ergo move wave# into LevelManager
+		AudioSource.PlayClipAtPoint (scuttle, transform.position);
+		levelManager.EnemyDown();
+		Destroy(this.gameObject, 0.001f);
+	}
+}
---
+++ 2D_Shooter/Assets/_Scripts/SpriteController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteController : MonoBehaviour {
+	public Sprite[] mySprite;  // adjust/set in inspector!
+
+	private int spriteIndex;
+	private int frameBuffer = 1;
+	private int frameCounter = 0;
+
+	void Start () { spriteIndex = 0; }
+
+	void LoadSprite () {
+		if (mySprite[spriteIndex]) { this.GetComponent<SpriteRenderer>().sprite = mySprite[spriteIndex]; } 
+		else { Debug.LogError ("SpriteController.cs ERROR: " + this + " spriteIndex " + spriteIndex + " mismatch."); }
+	}
+
+	void Update () {
+		if (frameCounter++ > frameBuffer) {
+			frameCounter = 0;
+			if (spriteIndex++ >= mySprite.Length -1) spriteIndex = 0; 
+			LoadSprite();
+		}
+	}
+}
