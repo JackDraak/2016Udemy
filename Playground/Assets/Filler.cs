4e1fa603ebe4bfca33a4a1f31364ea24fc61afb7
JackDraak <jackdraak@gmail.com>
2016-02-27 01:38:29 -0500

    Filler.cs - extending with reshuffle

--- Playground/Assets/Filler.cs
+++ Playground/Assets/Filler.cs
@@ -14,6 +14,21 @@ public class Filler : MonoBehaviour {
 		numberOfBoxes = textBox.Length;
 		numberOfFillers = filler.Length;
 
+		FillBoard();
+	}
+
+	/*
+		Note: if this were intended to be a Boggle board you should draw fillers 
+		from a struct(?) that gives each latter a score value and leans away from 
+		high scoring letters, i.e. Q & Z (letter frequency is inversely proprtional 
+		to it's score value; score 1 = most frequent, 10 = least.) 
+
+		ideas: 
+			- have a limit on duplicate high letters?
+			- have a "score range" that the sum of the board must fall within dependent
+				upon skill level? 
+	*/
+	public void FillBoard () {
 		for (int boxNumber = 0; boxNumber < numberOfBoxes ; boxNumber++) {
 			int fill = Random.Range (0, numberOfFillers);
 			textBox[boxNumber].text = filler[fill];
