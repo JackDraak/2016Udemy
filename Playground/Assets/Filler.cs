4c06ed18eacb126fce97dcfbed7828e17e0463ea
JackDraak <jackdraak@gmail.com>
2016-02-27 01:17:09 -0500

    Filler.cs - showing someone how to make a Boggle board of sorts

---
+++ Playground/Assets/Filler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Filler : MonoBehaviour {
+
+	public Text[] textBox;
+	public string[] filler;
+
+	private int numberOfBoxes;
+	private int numberOfFillers;
+
+	void Start () {
+		numberOfBoxes = textBox.Length;
+		numberOfFillers = filler.Length;
+
+		for (int boxNumber = 0; boxNumber < numberOfBoxes ; boxNumber++) {
+			int fill = Random.Range (0, numberOfFillers);
+			textBox[boxNumber].text = filler[fill];
+		}
+	}
+}
