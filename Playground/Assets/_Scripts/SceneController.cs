7d2627c8c908f22297121fe08ca9810466de5044
JackDraak <jackdraak@gmail.com>
2016-03-01 21:08:42 -0500

    housekeeping

---
+++ Playground/Assets/_Scripts/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	//public Button scene1, scene2, scene3;
+
+	public void Scene1Button () { SceneManager.LoadScene("scene1"); }
+	public void Scene2Button () { SceneManager.LoadScene("scene2"); }	
+	public void Scene3Button () { SceneManager.LoadScene("scene3"); }	
+}
---
+++ Playground/Assets/_Scripts/SplashManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashManager : MonoBehaviour {
+	public float autoLoadNextLevelDelay = 3;  // adjust/set in inspector!
+
+	void Start () {
+		Cursor.visible = false;
+		Invoke("LoadNextLevel", autoLoadNextLevelDelay);
+	}
+	public void LoadNextLevel() {
+		Cursor.visible = true;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+	}
+}
