55ad1453544cbf3674a4f92bbf084ebe42c609c7
JackDraak <jackdraak@gmail.com>
2016-03-12 13:05:58 -0500

    replacement splashsceen subcontroller LoadSlider.cs

---
+++ OLW-SkyDefender-Redux/Assets/_Scripts/LoadSlider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadSlider : MonoBehaviour {
+
+	public Slider slider;
+	public float loadTimeInSeconds = 20;
+
+	private float timeAwake;
+
+	void Awake () { float timeAwake = 0; }
+
+	void Update () {
+		if (timeAwake == 0) timeAwake = Time.time;
+		slider.value = (Time.time - timeAwake) / loadTimeInSeconds;
+	}
+}
