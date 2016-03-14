c6ea0505d0ba60a2052259d1c175f371a19adc08
JackDraak <jackdraak@gmail.com>
2016-03-14 00:33:56 -0400

    compiles, works

--- CaveGeneration/Assets/MeshGenerator.cs
+++ CaveGeneration/Assets/MeshGenerator.cs
@@ -9,10 +9,10 @@ public class MeshGenerator : MonoBehaviour {
 		squareGrid = new SquareGrid(map, squareSize);
 	}
 
-	void OnDrawGizmos () {
+	void OnDrawGizmos() {
 		if (squareGrid != null) {
-			for (int x = 0; x < squareGrid.squares.GetLength(0); x++) {
-				for (int y = 0; y < squareGrid.squares.GetLength(1); y++) {
+			for (int x = 0; x < squareGrid.squares.GetLength(0); x ++) {
+				for (int y = 0; y < squareGrid.squares.GetLength(1); y ++) {
 
 					Gizmos.color = (squareGrid.squares[x,y].topLeft.active)?Color.black:Color.white;
 					Gizmos.DrawCube(squareGrid.squares[x,y].topLeft.position, Vector3.one * .4f);
@@ -20,24 +20,24 @@ public class MeshGenerator : MonoBehaviour {
 					Gizmos.color = (squareGrid.squares[x,y].topRight.active)?Color.black:Color.white;
 					Gizmos.DrawCube(squareGrid.squares[x,y].topRight.position, Vector3.one * .4f);
 
-					Gizmos.color = (squareGrid.squares[x,y].bottomLeft.active)?Color.black:Color.white;
-					Gizmos.DrawCube(squareGrid.squares[x,y].bottomLeft.position, Vector3.one * .4f);
-
 					Gizmos.color = (squareGrid.squares[x,y].bottomRight.active)?Color.black:Color.white;
 					Gizmos.DrawCube(squareGrid.squares[x,y].bottomRight.position, Vector3.one * .4f);
 
+					Gizmos.color = (squareGrid.squares[x,y].bottomLeft.active)?Color.black:Color.white;
+					Gizmos.DrawCube(squareGrid.squares[x,y].bottomLeft.position, Vector3.one * .4f);
+
+
 					Gizmos.color = Color.grey;
 					Gizmos.DrawCube(squareGrid.squares[x,y].centreTop.position, Vector3.one * .15f);
-					Gizmos.DrawCube(squareGrid.squares[x,y].centreLeft.position, Vector3.one * .15f);
 					Gizmos.DrawCube(squareGrid.squares[x,y].centreRight.position, Vector3.one * .15f);
 					Gizmos.DrawCube(squareGrid.squares[x,y].centreBottom.position, Vector3.one * .15f);
-
+					Gizmos.DrawCube(squareGrid.squares[x,y].centreLeft.position, Vector3.one * .15f);
 
 				}
 			}
 		}
 	}
-
+	
 	public class SquareGrid {
 		public Square[,] squares;
 
@@ -47,39 +47,40 @@ public class MeshGenerator : MonoBehaviour {
 			float mapWidth = nodeCountX * squareSize;
 			float mapHeight = nodeCountY * squareSize;
 
-			ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+			ControlNode[,] controlNodes = new ControlNode[nodeCountX,nodeCountY];
 
-			for (int x = 0; x < nodeCountX; x++) {
-				for (int y = 0; y < nodeCountY; y++) {
+			for (int x = 0; x < nodeCountX; x ++) {
+				for (int y = 0; y < nodeCountY; y ++) {
 					Vector3 pos = new Vector3(-mapWidth/2 + x * squareSize + squareSize/2, 0, -mapHeight/2 + y * squareSize + squareSize/2);
-					controlNodes[x,y] = new ControlNode(pos, map[x,y] == 1, squareSize);
+					controlNodes[x,y] = new ControlNode(pos,map[x,y] == 1, squareSize);
 				}
 			}
 
-			squares = new Square [nodeCountX -1, nodeCountY -1];
-			for (int x = 0; x < nodeCountX; x++) {
-				for (int y = 0; y < nodeCountY; y++) {
+			squares = new Square[nodeCountX -1,nodeCountY -1];
+			for (int x = 0; x < nodeCountX-1; x ++) {
+				for (int y = 0; y < nodeCountY-1; y ++) {
 					squares[x,y] = new Square(controlNodes[x,y+1], controlNodes[x+1,y+1], controlNodes[x+1,y], controlNodes[x,y]);
 				}
 			}
+
 		}
 	}
-
-
+	
 	public class Square {
-		public ControlNode topLeft, topRight, bottomLeft, bottomRight;
-		public Node centreTop, centreRight, centreLeft, centreBottom;
 
-		public Square (ControlNode _topLeft, ControlNode _topRight, ControlNode _BottomLeft, ControlNode _bottomRight) {
+		public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+		public Node centreTop, centreRight, centreBottom, centreLeft;
+
+		public Square (ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft) {
 			topLeft = _topLeft;
 			topRight = _topRight;
-			bottomLeft = _BottomLeft;
 			bottomRight = _bottomRight;
+			bottomLeft = _bottomLeft;
 
 			centreTop = topLeft.right;
 			centreRight = bottomRight.above;
-			centreLeft = bottomLeft.above;
 			centreBottom = bottomLeft.right;
+			centreLeft = bottomLeft.above;
 		}
 	}
 
@@ -87,21 +88,21 @@ public class MeshGenerator : MonoBehaviour {
 		public Vector3 position;
 		public int vertexIndex = -1;
 
-		//constructor
 		public Node(Vector3 _pos) {
 			position = _pos;
 		}
 	}
 
 	public class ControlNode : Node {
+
 		public bool active;
 		public Node above, right;
 
-		// constructor
 		public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos) {
 			active = _active;
 			above = new Node(position + Vector3.forward * squareSize/2f);
 			right = new Node(position + Vector3.right * squareSize/2f);
 		}
+
 	}
 }
